77ccc09071ead379f9f5124e89bc2d96c3c17a43
Avinash <sc2459@njit.edu>
2022-01-31 10:24:51 -0500

    changed API_URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080";
-export const API_URL_Heroku = "https://ice-node-challenge.herokuapp.com";
+// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://ice-node-challenge.herokuapp.com";
 export const endPoint = "/handle";
